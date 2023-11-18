8963220f4797b1b0bd7dac4986f203564a49a1ab
Michael-Pham1 <miekolpham20@gmail.com>
2023-11-18 14:24:21 -0800

    added first + last name

--- gui.py
+++ gui.py
@@ -2,11 +2,12 @@ import customtkinter
 import getSpeech
 
 def login():
-    name = entry1.get()  # Get the text from entry1 (Name)
-    email = entry2.get()  # Get the text from entry2 (Email)
+    firstName = entry1.get()  # Get the text from entry1 (Name)
+    lastName = entry2.get()  # Get the text from entry1 (Name)
+    email = entry3.get()  # Get the text from entry2 (Email)
 
     # Check if both Name and Email fields are not empty
-    if name and email:
+    if firstName and lastName and email:
         root.withdraw()  # Hide the login window
 
         # Create a new window for the next page
@@ -17,7 +18,7 @@ def login():
             print("Recording data...")
             # Add functionality to record data or navigate to another page
             
-            getSpeech.main()
+            getSpeech.main(firstName, lastName, email)
 
         record_button = customtkinter.CTkButton(master=next_page, text="Record", command=record_data)
         record_button.pack(pady=20)
@@ -35,11 +36,14 @@ root.geometry("500x350")
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Name")
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="First Name")
 entry1.pack(pady=12, padx=10)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Email")
-entry2.pack(pady=12, padx=10)
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Last Name")
+entry3.pack(pady=12, padx=10)
+
+entry3 = customtkinter.CTkEntry(master=frame, placeholder_text="Email")
+entry3.pack(pady=12, padx=10)
 
 login_button = customtkinter.CTkButton(master=frame, text="Login", command=login)
 login_button.pack(pady=12, padx=10)
