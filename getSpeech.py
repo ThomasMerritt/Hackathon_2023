3525f46d45461fff299fcb12f6fb01b17efa2b8e
DerpyMissile <hmai015@ucr.edu>
2023-11-18 13:12:56 -0800

    Update getSpeech.py

--- getSpeech.py
+++ getSpeech.py
@@ -10,6 +10,7 @@ recognizer = speech_recognition.Recognizer()
 #defs
 def main():
     timesSworn = 0
+    firstPass = True
     with open("badwords.txt", "r") as nono:
         lines = nono.readlines()
         badwords.extend([word.strip().lower() for line in lines for word in line.split("\n") if word.strip()])
@@ -31,10 +32,15 @@ def main():
         #     sendMail.sendSpam(userName, userLastName, userEmail, timesSworn)
         #     print("THAT'S A BAD WORD!")
         #     result = ""
+        firstPass = True
         for word in badwords:
             temp = result.lower().count(word)
             if temp > 0:
-                timesSworn += 1
+                if firstPass:
+                    timesSworn += 1
+                    firstPass = False
+                else:
+                    timesSworn += 0
                 sendMail.sendSpam(userName, userLastName, userEmail, timesSworn)
                 print("THAT'S A BAD WORD!")
                 result = ""
