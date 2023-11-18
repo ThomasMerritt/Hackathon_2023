0b66f0110916e93236f087ea4b5afcf1acd947c2
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2023-11-18 13:42:19 -0800

    Check OS and file adding/ deletion
    Check the OS to ensure proper file directory. Copy history file into the folder and delete after the process is complete

--- db_extraction.py
+++ db_extraction.py
@@ -1,4 +1,20 @@
 import sqlite3
+import os
+import shutil
+import platform
+
+
+# Get the os for the proper file directory
+if (platform.system() == 'Darwin'):
+    source_path = '/Users/' + os.getlogin() + '/library/applicationsupport/google/chrome/default/history'
+elif (platform.system() == 'Windows'):
+    source_path = 'C:/Users/' + os.getlogin() + '/AppData/Local/Google/Chrome/UserData/Default'
+
+
+current_directory = os.getcwd() + "/history"
+
+
+shutil.copyfile(source_path, current_directory)
 
 connect = sqlite3.connect('history')
 # connect = sqlite3.connect('/Users/thomasmerritt/library/applicationsupport/google/chrome/default/history')
@@ -18,6 +34,12 @@ for keyword in keywords:
 # Process the results (e.g., print them)
 for row in truth:
     print(row)
+
+if (os.path.exists(current_directory)):
+    os.remove(current_directory)
+    print("File deleted successfully.")
+else:
+    print("File does not exist")
 
 # Close the connection
 connect.close()
